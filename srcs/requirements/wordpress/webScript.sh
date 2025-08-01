b61ed839fd2b6f1842bcc07f1c8c7e687661fc7f
Quan Anh Nguyen <93729938+Inzagini@users.noreply.github.com>
2025-08-01 14:28:42 +0200

    working condition

--- srcs/requirements/wordpress/webScript.sh
+++ srcs/requirements/wordpress/webScript.sh
@@ -8,7 +8,7 @@ else
 	echo "WordPress not configured. Starting setup..."
 	echo "Waiting for MariaDB to be ready..."
 	for i in {1..6}; do
-		if mysql -h mariadb -u ${MYSQL_USER} -p${MYSQL_PASSWORD} -e "SELECT 1" >/dev/null 2>&1; then
+		if mysql -h mariadb -u ${MYSQL_USER} -p$(cat ${MYSQL_PASSWORD_FILE}) -e "SELECT 1" >/dev/null 2>&1; then
 			echo "MariaDB is ready!"
 			break
 		fi
@@ -19,8 +19,8 @@ else
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	./wp-cli.phar core download --allow-root
-	./wp-cli.phar config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
-	./wp-cli.phar core install --url=${URL_DOMAIN} --title=inception --admin_user=${MYSQL_ROOT_USER} --admin_password=${MYSQL_ROOT_PASSWORD} --admin_email=admin@admin.com --allow-root
+	./wp-cli.phar config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=$(cat ${MYSQL_PASSWORD_FILE}) --dbhost=mariadb --allow-root
+	./wp-cli.phar core install --url=${URL_DOMAIN} --title=inception --admin_user=${MYSQL_ROOT_USER} --admin_password=$(cat ${MYSQL_ROOT_PASSWORD_FILE}) --admin_email=admin@admin.com --allow-root
 	echo "Wordpress setup complete"
 fi
 
